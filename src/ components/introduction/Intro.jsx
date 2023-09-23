088f30992ba57411c4947643388488ee03619595
Gagan Gowda N <gagangowda7687@gmail.com>
2023-09-23 13:34:47 +0530

    add languages

--- src/ components/introduction/Intro.jsx
+++ src/ components/introduction/Intro.jsx
@@ -1,20 +1,32 @@
-import React from 'react'
-import './Intro.css'
-import {Link} from 'react-scroll'
-import background from '../../assets/pic.png'
-import buttonImg from '../../assets/hireme.png'
+import React from "react";
+import "./Intro.css";
+import background from "../../assets/pic.png";
+import Typewriter from "typewriter-effect";
 function Intro() {
   return (
-    <section id='intro'>
-          <div className="introContent">
-              <span className="hello">Hello </span>
-              <span className="introText">I'm  <span className="name"> Gagan</span></span>
-              <span className='description'>WebSite Designer</span>
-              <p className='introPara'>I am a skilled web developer with experience in creating multiple projects</p>
-          </div>
-          <img src={background} alt="pic" className='pic'/>
+    <section id="intro">
+      <div className="introContent">
+        <span className="hello">Hello </span>
+        <span className="introText">
+          I'm <span className="name"> Gagan</span>
+        </span>
+        <span className="description">WebSite Designer</span>
+        <p className="introPara">
+          <Typewriter
+            onInit={(typewriter) => {
+              typewriter
+                .typeString(
+                  "I am a skilled web developer with experience in creating multiple projects"
+                )
+                .changeDelay(2)
+                .start();
+            }}
+          />
+        </p>
+      </div>
+      <img src={background} alt="pic" className="pic" />
     </section>
-  )
+  );
 }
 
-export default Intro
+export default Intro;
--- src/ components/skills/Skill.jsx
+++ src/ components/skills/Skill.jsx
@@ -1,20 +1,31 @@
 import React, { useState } from "react";
 import "./skill.css";
-import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
-import ExpandLessIcon from '@mui/icons-material/ExpandLess';
+import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
+import ExpandLessIcon from "@mui/icons-material/ExpandLess";
 import UIDesign from "../../assets/ui-design.png";
+import react from "../../assets/react.png";
+import js from "../../assets/js.png";
+import css from "../../assets/css.png";
+import html from "../../assets/html.png";
+import git from "../../assets/git.png";
+import vs from "../../assets/vs.png";
+import eclipse from "../../assets/eclipse.png";
+import ij from "../../assets/ij.png";
+import mac from "../../assets/mac.png";
+import window from "../../assets/window.png";
+import material from "../../assets/material.png";
 import WebDesign from "../../assets/website-design.png";
 import AppDesign from "../../assets/app-design.png";
-import os from '../../assets/os.png';
+import os from "../../assets/os.png";
 function Skill() {
   const [openSkills, setOpenSkills] = useState({});
   const skills = {
-    "Web-Technologies": ["React", "JavaScript", "Css", "HTML", "Material UI"],
-    "Version Control": ["GitHub"],
-    Tools: ["VisualStudio", "Eclipse", "IntelliJ"],
-    "Operating Systems": ["MacOs", "Windows"],
+    "Web-Technologies": {"React":react, "JavaScript":js, "Css":css, "HTML":html, "Material UI":material},
+    "Version Control": {"GitHub":git},
+    Tools: {"VisualStudio":vs, "Eclipse":eclipse, "IntelliJ":ij},
+    "Operating Systems": {"MacOs":mac, "Windows":window},
   };
-  const skillsImages = [WebDesign, UIDesign, AppDesign,os];
+  const skillsImages = [WebDesign, UIDesign, AppDesign, os];
 
   const toggleSkill = (skillCategory) => {
     setOpenSkills((prevState) => ({
@@ -34,18 +45,26 @@ function Skill() {
       <div className="skillBars">
         {Object.keys(skills).map((row, index) => (
           <>
-            <div className="skillBar" key={row} onClick={() => toggleSkill(row)}>
+            <div
+              className="skillBar"
+              key={row}
+              onClick={() => toggleSkill(row)}
+            >
               <img
                 src={skillsImages[index]}
                 alt="WebDesign"
                 className="skillBarImage"
               />
-              <div className="skillBarText" >
+              <div className="skillBarText">
                 <h2>{row}</h2>
               </div>
-             {!openSkills[row]? <ExpandMoreIcon className="dropDown" fontSize='large'/>:<ExpandLessIcon className="dropDown" fontSize='large'/>}
+              {!openSkills[row] ? (
+                <ExpandMoreIcon className="dropDown" fontSize="large" />
+              ) : (
+                <ExpandLessIcon className="dropDown" fontSize="large" />
+              )}
             </div>
-            {openSkills[row] && <Skillset keys={skills} row={row} />}
+            {openSkills[row] && <Skillset keys={skills} row={row} skills={skills}/>}
           </>
         ))}
       </div>
@@ -53,23 +72,30 @@ function Skill() {
   );
 }
 
-function Skillset({ keys, row }) {
+function Skillset({ keys, row,skills }) {
   return (
-    <>
-      {keys[row]?.map((skill) => (
-        <Skillist row={skill} key={skill} />
+    <div className="skillListBars">
+    <div className="skillListBar">
+      {Object.keys(keys[row])?.map((skill,index) => (
+        <Skillist row={skill} key={skill} skills={skills[row]}/>
       ))}
-    </>
+   </div>
+    </div>
   );
 }
 
-function Skillist({ row }) {
+function Skillist({ row ,skills}) {
   return (
-    <div className="skillListBar">
-      <div className="skillBarText">
+
+      <div className="skillBarChildText">
+      <img
+                src={skills[row]}
+                alt="WebDesign"
+                className="skilllanguages"
+              />
         <h2>{row}</h2>
       </div>
-    </div>
+  
   );
 }
 
