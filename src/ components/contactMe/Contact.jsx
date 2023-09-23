73353c8bd40708501fdf100203bd2f84610476f4
ggowda <ggowda@zuora.com>
2023-09-23 10:30:02 +0530

    add validation

--- src/ components/contactMe/Contact.jsx
+++ src/ components/contactMe/Contact.jsx
@@ -1,34 +1,65 @@
-import { React, useRef } from "react";
+import { React, useRef, useState, useEffect } from "react";
 import "./contact.css";
 import VerticalAlignTopRoundedIcon from "@mui/icons-material/VerticalAlignTopRounded";
 import linkedin from "../../assets/linkedin.png";
 import instagram from "../../assets/instagram.png";
 import emailjs from "@emailjs/browser";
 import { Tooltip } from "@mui/material";
-import {Link} from 'react-scroll'
+import { Link } from "react-scroll";
 function Contact() {
+  let [details, setDetails] = useState({
+    from_name: "",
+    from_email: "",
+    message: "",
+  });
+  const [error, setError] = useState(false);
+  const handleChange = (event) => {
+    let value = event.target.value;
+    let name = event.target.name;
+
+    setDetails((prevalue) => {
+      return {
+        ...prevalue,
+        [name]: value,
+      };
+    });
+  };
   const form = useRef();
   const sendEmail = (e) => {
     e.preventDefault();
-
-    emailjs
-      .sendForm(
-        "service_je8pxw6",
-        "template_pyiqscq",
-        form.current,
-        "r_zQpf2MO7hNCOwh8"
-      )
-      .then(
-        (result) => {
-          console.log(result.text);
-          alert("Email Sent !");
-          e.target.reset();
-        },
-        (error) => {
-          console.error(error.text);
-        }
-      );
+    if (!details.from_name || !details.from_email || !details.message) {
+      setError(true);
+    } else {
+      emailjs
+        .sendForm(
+          "service_je8pxw6",
+          "template_pyiqscq",
+          form.current,
+          "r_zQpf2MO7hNCOwh8"
+        )
+        .then(
+          (result) => {
+            console.log(result.text);
+            alert("Email Sent !");
+            e.target.reset();
+            setDetails({
+                from_name: "",
+                from_email: "",
+                message: "",
+              })
+          },
+          (error) => {
+            console.error(error.text);
+          }
+        );
+    }
   };
+
+  useEffect(() => {
+    if (details.from_name && details.from_email && details.message)
+      setError(false);
+  }, [details]);
+
   return (
     <section id="contact">
       <h1 className="contactPageTitle">Contact Me</h1>
@@ -41,12 +72,14 @@ function Contact() {
           className="yourname"
           placeholder="Your name"
           name="from_name"
+          onChange={handleChange}
         />
         <input
           type="email"
           className="email"
           placeholder="Your email"
           name="from_email"
+          onChange={handleChange}
         />
         <textarea
           name="message"
@@ -54,8 +87,15 @@ function Contact() {
           rows="5"
           className="message"
           placeholder="Your message"
+          onChange={handleChange}
         />
-        <button className="submit" type="submit" value="Send">
+        {error && <span className="error">Please fill the above form completely</span>}
+        <button
+          className="submit"
+          type="submit"
+          Tooltip="Input the values"
+          value="Send"
+        >
           Submit
         </button>
         <div className="links">
